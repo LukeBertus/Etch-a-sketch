6bf0c78091d984853d83c918b06965520525dd0e
LukeBertus <vboxuser@Xubuntu2.myguest.virtualbox.org>
2023-05-23 18:37:18 +0800

    Complete grid logic

--- script.js
+++ script.js
@@ -3,11 +3,17 @@ let gridSize = 16;
 const canvas = document.getElementById("canvas")
 
 createGrid = (size) => {
-    for (i=0; i<size; i++) {
-        const pixel = document.createElement("div");
-        pixel.className = "pixelRow"
-        canvas.appendChild(pixel);
+    for (let i=0; i<size; i++) {
+        const pixelRow = document.createElement("div");
+        pixelRow.className = "pixelRow";
+        canvas.appendChild(pixelRow);
+            for (let i=0; i<size; i++) {
+                const pixel = document.createElement("div");
+                pixel.className = "pixel";
+                pixelRow.appendChild(pixel);
+                console.log("hi");
+            }
     }
 }
 
-createGrid(7);
+createGrid(3);
