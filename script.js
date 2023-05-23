4fc9ad92163d805fed11e6ca738da22fc3b6655b
LukeBertus <vboxuser@Xubuntu2.myguest.virtualbox.org>
2023-05-23 19:53:41 +0800

    Add drawing logic

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-let gridSize = 16;
 
 const canvas = document.getElementById("canvas")
+let isMouseDown = "false";
+const page = document.querySelector("body")
+
+page.addEventListener('mousedown', e => {
+    isMouseDown = "true";
+    console.log(isMouseDown);
+})
+page.addEventListener('mouseup', e => {
+    isMouseDown = "false";
+    console.log(isMouseDown);
+})
 
 createGrid = (size) => {
     for (let i=0; i<size; i++) {
@@ -9,6 +19,12 @@ createGrid = (size) => {
         canvas.appendChild(pixelRow);
             for (let i=0; i<size; i++) {
                 const pixel = document.createElement("div");
+                pixel.addEventListener("mousedown", e => {
+                    pixel.className += " coloured"})
+                pixel.addEventListener("mouseover", (e) => {
+                    console.log(isMouseDown);
+                    if (isMouseDown == "true") {
+                    pixel.className += " coloured"}})
                 pixel.className = "pixel";
                 pixelRow.appendChild(pixel);
                 console.log("hi");
@@ -16,4 +32,4 @@ createGrid = (size) => {
     }
 }
 
-createGrid(3);
+createGrid(16);
